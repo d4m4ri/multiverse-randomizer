5f701a349bf91a38c4830d1d7868c70f47fbfc3f
Daniel <d4m4ri@users.noreply.github.com>
2018-02-01 18:30:01 +0100

    Fix build scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-./gradlew jar
-java -Xmx4096m -Xms4096m -XX:+CMSClassUnloadingEnabled -XX:+HeapDumpOnOutOfMemoryError -jar build/libs/*.jar
+java -Xmx4096m -Xms4096m -XX:+CMSClassUnloadingEnabled -XX:+HeapDumpOnOutOfMemoryError -jar build/libs/multiverse-randomizer-0.1.jar
